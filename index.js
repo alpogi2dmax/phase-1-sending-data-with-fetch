3e707525c1943cc806177746b5a5ebd5e7da90e9
Al Cerdan Lico <alcerdan@yahoo.com>
2024-09-23 13:06:21 -0700

    completed sending-data-with-fetch

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({ name: name, email: email })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("Data:", data);
+            const newId = data.id;
+            const body = document.body;
+            const p = document.createElement('p');
+            p.textContent = newId;
+            body.appendChild(p);
+        })
+    .catch(error => {
+        const body = document.body;
+        const p = document.createElement('p');
+        p.innerHTML = error.message;
+        body.appendChild(p);
+    })
+}
+
+
